bb8d614164978ce790f0585304eeb9b0419a28a9
fyurchik <jyrafenun@gmail.com>
2025-03-14 14:53:27 +0200

    refactoring

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,54 +1,88 @@
-class GildedRose
+class ItemType
+  def update(item)
+    update_quality(item)
+    update_sell_in(item)
+    update_quality(item) if expired?(item)
+  end
 
+  protected
+
+  def update_quality(item)
+    item.quality -= 1 if item.quality > 0
+  end
+
+  def update_sell_in(item)
+    item.sell_in -= 1
+  end
+  def expired?(item)
+    item.sell_in < 0
+  end
+end
+
+class AgedBrie < ItemType
+  protected
+
+  def update_quality(item)
+    item.quality += 1 if item.quality< 50
+  end
+end
+
+class BackstagePass < ItemType
+
+  protected
+
+  def update_quality(item)
+    if item.sell_in<= 0
+      item.quality = 0
+    elsif item.sell_in <= 5
+      item.quality += 3
+    elsif item.sell_in <= 10
+     item.quality += 2
+    else
+      item.quality +=1
+    end
+    item.quality =[item.quality, 50].min
+  end
+end
+
+class Sulfuras < ItemType
+  def update(item)
+    #nohing to change
+  end
+end
+
+class Conjured < ItemType
+  protected
+
+  def update_quality(item)
+    2.times {super}
+  end
+end
+
+class NormalItem < ItemType
+end
+
+class ItemTypeFactory
+  ITEM_TYPES = {
+    "Aged Brie"                                 => AgedBrie,
+    "Backstage passes to a TAFKAL80ETC concert" => BackstagePass,
+    "Sulfuras, Hand of Ragnaros"                => Sulfuras,
+    "Conjured"                                  => Conjured
+  }
+
+  def self.create(item)
+    (ITEM_TYPES[item.name] || NormalItem).new
+  end
+end
+
+class GildedRose
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+     ItemTypeFactory.create(item).update(item)
     end
   end
 end
--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -3,9 +3,24 @@ require 'rspec'
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
-  it "does not change the name" do
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    expect(items[0].name).to eq "fixme"
+  describe "#update_quality" do
+    let(:items)       { [Item.new("Aged Brie", 5, 10), Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 20), Item.new("Conjured", 3, 6)] }
+    let(:gilded_rose) { GildedRose.new(items) }
+
+    
+    it "updates for Aged Brie" do
+      gilded_rose.update_quality 
+      expect(items[0].quality).to eq(11)
+    end
+
+    it "updates for Backstage " do
+      gilded_rose.update_quality
+      expect(items[1].quality).to eq(22)
+    end
+
+    it "updatefor Conjured" do
+      gilded_rose.update_quality
+      expect(items[2].quality).to eq(4)
+    end
   end
 end
